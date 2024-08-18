9db75a5042e8a8cf6ef6bd3afde78ca9dd8f7087
njamster <njamster@users.noreply.github.com>
2024-08-18 17:32:29 +0200

    Spawn boxes at the mouse cursor's x-position

--- screens/game.gd
+++ screens/game.gd
@@ -5,15 +5,10 @@ const BASIC_BLOCK := preload("res://blocks/basic_block.tscn")
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.is_released():
-		if event.button_index == MOUSE_BUTTON_LEFT:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_released():
 			var block := BASIC_BLOCK.instantiate()
-			block.global_position.x = randf_range(242, 778)
-			block.global_position.y = -100
-			$Blocks.add_child(block)
-		if event.button_index == MOUSE_BUTTON_RIGHT:
-			var block := BASIC_BLOCK.instantiate()
-			block.global_position.x = randf_range(1142, 1678)
+			block.global_position.x = event.global_position.x
 			block.global_position.y = -100
 			$Blocks.add_child(block)
 
