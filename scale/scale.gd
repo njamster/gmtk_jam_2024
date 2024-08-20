8c114f8163c410388b123e7044df9cde4a2d83f6
njamster <njamster@users.noreply.github.com>
2024-08-20 11:51:26 +0200

    Rework & simplify the scale behavior
    - Whatever side is heavier will now (slowly!) move downwards without
      being limited to a certain threshold.
    - The bigger the weight difference between both sides the faster the
      trays will move up and down.

--- scale/scale.gd
+++ scale/scale.gd
@@ -1,38 +1,30 @@
 extends Node2D
 
 
-const MAX_HEIGHT := 500  # pixels
-const DEFAULT_WEIGHT := 20.0  # kilogram
-const MOVE_SPEED := 60.0  # pixels per second
+const REBALANCE_SPEED := 60.0  # pixels per second
 
-var total_weight_L := DEFAULT_WEIGHT
-var total_weight_R := DEFAULT_WEIGHT
+var total_weight_L := 0
+var total_weight_R := 0
 
 
 func _physics_process(delta: float) -> void:
+	# step 1: move the trays
 	if total_weight_L == total_weight_R:
 		# return to equilibrium
-		%LeftSide.position.y = move_toward(%LeftSide.position.y, 0, MOVE_SPEED * delta)
-		%RightSide.position.y = move_toward(%RightSide.position.y, 0, MOVE_SPEED * delta)
-	elif total_weight_L > total_weight_R:
-		# move left side up & right side down
-		var weight_ratio = total_weight_L / total_weight_R
-		var height = clamp((weight_ratio - 1.0) * 100, 0, MAX_HEIGHT)
-		var speed = weight_ratio * MOVE_SPEED * delta
-		%LeftSide.position.y = move_toward(%LeftSide.position.y, height, speed)
-		%RightSide.position.y = move_toward(%RightSide.position.y, -1 * height, speed)
-	elif total_weight_L < total_weight_R:
-		# move right side up & left side down
-		var weight_ratio = total_weight_R / total_weight_L
-		var height = clamp((weight_ratio - 1.0) * 100, 0, MAX_HEIGHT)
-		var speed = weight_ratio * MOVE_SPEED * delta
-		%LeftSide.position.y = move_toward(%LeftSide.position.y, -1 * height, speed)
-		%RightSide.position.y = move_toward(%RightSide.position.y, height, speed)
+		%LeftSide.position.y = move_toward(%LeftSide.position.y, 0, REBALANCE_SPEED * delta)
+		%RightSide.position.y = move_toward(%RightSide.position.y, 0, REBALANCE_SPEED * delta)
+	else:
+		var weight_difference := total_weight_L - total_weight_R
+		%LeftSide.position.y += 0.5 * weight_difference * delta
+		%RightSide.position.y -= 0.5 * weight_difference * delta
 
+	# step 2: rotate the connector
 	$Connector.rotation = $LeftSide.position.direction_to($RightSide.position).angle()
 
-	$LeftSide/Total.text = str(total_weight_L - DEFAULT_WEIGHT)
-	$RightSide/Total.text = str(total_weight_R - DEFAULT_WEIGHT)
+	# step 3: update the total weight display
+	$LeftSide/Total.text = str(total_weight_L)
+	$RightSide/Total.text = str(total_weight_R)
 
-	total_weight_L = DEFAULT_WEIGHT
-	total_weight_R = DEFAULT_WEIGHT
+	# step 4: reset the total weights (will be recalculated next frame)
+	total_weight_L = 0
+	total_weight_R = 0
