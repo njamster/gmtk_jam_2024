41e0e01e2c61eaffdf500f2e721e09cbb89437d7
njamster <njamster@users.noreply.github.com>
2024-08-20 16:45:19 +0200

    Customize mouse cursor

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -1,6 +1,7 @@
 extends Node
 
 
+
 func take_screenshot() -> void:
 	var capture = get_viewport().get_texture().get_image()
 	var _time = Time.get_datetime_string_from_system()
@@ -11,3 +12,17 @@ func take_screenshot() -> void:
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.key_label == KEY_F12 and event.pressed:
 		take_screenshot()
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			Input.set_custom_mouse_cursor(
+				preload("res://theme/mouse_cursors/hand_point_click.png"),
+				Input.CURSOR_ARROW, Vector2(10, 5)
+			)
+		else:
+			Input.set_custom_mouse_cursor(
+				preload("res://theme/mouse_cursors/hand_point.png"),
+				Input.CURSOR_ARROW, Vector2(8, 7)
+			)
