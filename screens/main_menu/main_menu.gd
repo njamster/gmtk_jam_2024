e1c3c7c5a609f70329113b8bb351a018d519bca8
njamster <njamster@users.noreply.github.com>
2024-08-20 15:40:56 +0200

    Brush up the main menu even more

--- screens/main_menu/main_menu.gd
+++ screens/main_menu/main_menu.gd
@@ -1,7 +1,12 @@
-extends ColorRect
+extends Control
 
 
 const BASIC_BLOCK := preload("res://blocks/basic_block.tscn")
+
+
+func _ready() -> void:
+	_on_spawn_timer_timeout()
+	%Play.grab_focus()
 
 
 func _on_play_pressed() -> void:
@@ -9,11 +14,11 @@ func _on_play_pressed() -> void:
 
 
 func _on_spawn_timer_timeout() -> void:
-	const NUM_BLOCKS := 4.0
+	const NUM_BLOCKS := 5.0
 	for i in range(NUM_BLOCKS):
 		var block := BASIC_BLOCK.instantiate()
 		block.global_position.x = randf_range(i * 1920/NUM_BLOCKS, (i+1) * 1920/NUM_BLOCKS)
 		block.global_position.y = randf_range(-100, -400)
-		block.rotation_degrees = randf_range(-20, 20)
+		block.rotation_degrees = randi_range(-20, 20)
 		block.state = block.States.MENU_SCREEN
 		$Blocks.add_child(block)
