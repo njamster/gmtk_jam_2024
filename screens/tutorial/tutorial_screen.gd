381a2f317b319963282c61cf05dd8a72c091839a
njamster <njamster@users.noreply.github.com>
2024-08-20 19:34:44 +0200

    Improve the tutorial in various ways

--- screens/tutorial/tutorial_screen.gd
+++ screens/tutorial/tutorial_screen.gd
@@ -1,5 +1,24 @@
 extends Control
 
 
+
+func _enter_tree() -> void:
+	%Part1.visible_ratio = 1
+	%Part2.visible_ratio = 0
+	%Part3.visible_ratio = 0
+
+	if OS.has_feature("web_android") or OS.has_feature("web_ios"):
+		# if runnig the web version on a mobile device, adapt the tutorial
+		%Part2.text = %Part2.text.replace("Move the mouse", "Press and hold")
+		%Part2.text = %Part2.text.replace("Click to", "Release to")
+
+
 func _on_continue_pressed() -> void:
-	get_tree().change_scene_to_file("res://screens/game/game.tscn")
+	if %Part1.visible_ratio != 1:
+		%Part1.visible_ratio = 1
+	elif %Part2.visible_ratio != 1:
+		%Part2.visible_ratio = 1
+	elif %Part3.visible_ratio != 1:
+		%Part3.visible_ratio = 1
+	else:
+		get_tree().change_scene_to_file("res://screens/game/game.tscn")
