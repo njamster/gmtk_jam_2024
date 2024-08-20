2bdbbd7c583cad1c5cc79aff8a2591a88ce5c66b
njamster <njamster@users.noreply.github.com>
2024-08-20 12:17:34 +0200

    Fix `_is_grounded` not updating properly
    ... if landing on a block that isn't grounded itself yet.
    
    - Also, reduces the AirTimer's `wait_time` significantly from 0.5 to
      0.1 seconds, so that the total weight will update quicker. This was
      not possible previously when the trays would still move faster.

--- blocks/basic_block.gd
+++ blocks/basic_block.gd
@@ -36,9 +36,7 @@ func kill() -> void:
 	queue_free()
 
 
-func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("Block") and not body._is_grounded:
-		return
+func _on_body_entered(_body: Node) -> void:
 	$AirTimer.stop()
 	_is_grounded = true
 
@@ -47,7 +45,7 @@ func _on_body_exited(_body: Node) -> void:
 	$AirTimer.start()
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(_ydelta: float) -> void:
 	if _is_grounded:
 		if position.x < 960:
 			get_tree().get_root().get_node("Game/Scale").total_weight_L += mass
